86cb0e2059758a8a1a02e7b42d05e5e973e31313
khiz125 <ken.hiro118@gmail.com>
2025-08-23 16:40:43 +0900

    updated

--- comments/app.ts
+++ comments/app.ts
@@ -1,51 +1,71 @@
-import { Hono } from 'hono';
-import { logger } from 'hono/logger';
+import { Hono } from "hono";
+import { logger } from "hono/logger";
 
-import { randomBytes } from 'crypto';
-import { cors } from 'hono/cors';
+import { randomBytes } from "crypto";
+import { cors } from "hono/cors";
 
 type Comments = {
   id: string;
   comments: string;
-}
+};
 
 type CommentsByPostId = {
-  [key: string]: Comments[]
-}
+  [key: string]: Comments[];
+};
 
 const app = new Hono();
 
-const commentsByPostId: CommentsByPostId  = {};
+const commentsByPostId: CommentsByPostId  { };
 
-app.use('*', logger());
+app.use("*", logger());
 app.use(
-  '/posts/*',
+  "/posts/*",
   cors({
-    origin: ['http://localhost:5173','http://localhost:4000','http://localhost:3000'],
-  })
-)
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:4000",
+      "http://localhost:3000",
+    ],
+  }),
+);
 
-app.get('/posts/:id/comments', (c): ReturnType<typeof c.json<Result<Comments[]>>> => {
-  const postId = c.req.param('id');
-  if (!commentsByPostId[postId] || c.error) {
-    return c.json({ ok: false, error: "post id is invalid." }, 500);
-  }
-  const comments = commentsByPostId[postId];
-  return c.json({ ok: true, value: comments }, 200);
-});
+app.get(
+  "/posts/:id/comments",
+  (c): ReturnType<typeof c.json<Result<Comments[]>>> => {
+    const postId = c.req.param("id");
+    if (!commentsByPostId[postId] || c.error) {
+      return c.json({ ok: false, error: "post id is invalid." }, 500);
+    }
+    const comments = commentsByPostId[postId];
+    return c.json({ ok: true, value: comments }, 200);
+  },
+);
 
-app.post('/posts/:id/comments', async (c): Promise<ReturnType<typeof c.json<Result<Comments[]>>>>  => {
-  const postId = c.req.param('id');
-  if (!commentsByPostId[postId] || c.error) {
-    return c.json({ ok: false, error: "post id is invalid." }, 500);
-  }
-  const commentData = await c.req.json();
-  const commentId = randomBytes(4).toString('hex');
-  const comments = commentsByPostId[postId] = commentData.comments || [];
-  comments.push({ id: commentId, commentData });
-  commentsByPostId[postId] = comments;
-  return c.json({ ok: true, value: comments }, 201);
-})
+app.post(
+  "/posts/:id/comments",
+  async (c): Promise<ReturnType<typeof c.json<Result<Comments[]>>>> => {
+    const postId = c.req.param("id");
+    if (!commentsByPostId[postId] || c.error) {
+      return c.json({ ok: false, error: "post id is invalid." }, 500);
+    }
+    const commentData = await c.req.json();
+    const commentId = randomBytes(4).toString("hex");
+    const comments = (commentsByPostId[postId] = commentData.comments || []);
+    comments.push({ id: commentId, commentData });
+    commentsByPostId[postId] = comments;
+
+    await fetch("http://event-bus-srv:4005/events", {
+      method: "POST",
+      body: JSON.stringify({
+        id: commentId,
+        comments,
+        postId: postId,
+        status: "pending"
+      })
+    });
+    return c.json({ ok: true, value: comments }, 201);
+  },
+);
 
 export default app;
 
@@ -53,4 +73,3 @@ export type Ok<T> = { ok: true; value: T };
 export type Err<E = string> = { ok: false; error: E };
 
 export type Result<T, E = string> = Ok<T> | Err<E>;
-
